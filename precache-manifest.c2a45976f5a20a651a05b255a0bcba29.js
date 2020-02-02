a19733df105c3ad0926dec7bc1f4683d859ee24a
Jaxon Burningham <jaxonburn@gmail.com>
2020-02-02 10:29:31 -0700

    Updates

---
+++ precache-manifest.c2a45976f5a20a651a05b255a0bcba29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f58e7428e7ac1d1ee47bf006af2601",
+    "url": "/Ecommerce/index.html"
+  },
+  {
+    "revision": "10a1d893bc5e47b7db6f",
+    "url": "/Ecommerce/static/css/main.0d5c250f.chunk.css"
+  },
+  {
+    "revision": "4be0e033e66d046e318f",
+    "url": "/Ecommerce/static/js/2.d1fdf2f4.chunk.js"
+  },
+  {
+    "revision": "f5ca32966347038a998bbf09dda684c7",
+    "url": "/Ecommerce/static/js/2.d1fdf2f4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10a1d893bc5e47b7db6f",
+    "url": "/Ecommerce/static/js/main.d3062933.chunk.js"
+  },
+  {
+    "revision": "d93d148ba58c538e932a",
+    "url": "/Ecommerce/static/js/runtime-main.0e09e238.js"
+  },
+  {
+    "revision": "46bf59119945a3d1bddae24cfb7b3b9a",
+    "url": "/Ecommerce/static/media/complete.46bf5911.jpg"
+  },
+  {
+    "revision": "32c216bab03e70afb3855b87a04dbbaf",
+    "url": "/Ecommerce/static/media/reactImage.32c216ba.jpg"
+  }
+]);
